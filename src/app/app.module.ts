efb7f56d4f14bbb0daf27054dc9c21a849dd0ab9
dri14maretti <dri.cmaretti@gmail.com>
2021-06-21 16:51:19 -0300

    mudança no forms

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import '@angular/compiler';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -25,6 +27,7 @@ import { MatButtonModule } from '@angular/material/button';
 		MatCardModule,
 		MatFormFieldModule,
 		MatButtonModule,
+		FormsModule,
 	],
 	providers: [],
 	bootstrap: [AppComponent],
