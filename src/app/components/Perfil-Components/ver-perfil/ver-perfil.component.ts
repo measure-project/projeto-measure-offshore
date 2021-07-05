710064e2de925e50253d447f08940a671ca320f1
dri14maretti <dri.cmaretti@gmail.com>
2021-07-05 17:14:39 -0300

    importando authService no novo componente

--- src/app/components/Perfil-Components/ver-perfil/ver-perfil.component.ts
+++ src/app/components/Perfil-Components/ver-perfil/ver-perfil.component.ts
@@ -1,15 +1,15 @@
+import { AuthService } from './../../../services/auth.service';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-ver-perfil',
-  templateUrl: './ver-perfil.component.html',
-  styleUrls: ['./ver-perfil.component.scss']
+	selector: 'app-ver-perfil',
+	templateUrl: './ver-perfil.component.html',
+	styleUrls: ['./ver-perfil.component.scss'],
 })
 export class VerPerfilComponent implements OnInit {
+	constructor(private authService: AuthService) {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+	ngOnInit(): void {
+		this.authService.displayMessage('Teste realizado com sucesso!', false);
+	}
 }
