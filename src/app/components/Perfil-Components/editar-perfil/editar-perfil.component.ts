2bc54b6bfdcd451aa6acf24bb2a1f3ce7d92754a
dri14maretti <dri.cmaretti@gmail.com>
2021-07-09 09:12:58 -0300

    mudanças no editar perfil

--- src/app/components/Perfil-Components/editar-perfil/editar-perfil.component.ts
+++ src/app/components/Perfil-Components/editar-perfil/editar-perfil.component.ts
@@ -1,15 +1,25 @@
+import { User } from './../../../models/user';
+import { Router } from '@angular/router';
+import { AuthService } from './../../../services/auth.service';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-editar-perfil',
-  templateUrl: './editar-perfil.component.html',
-  styleUrls: ['./editar-perfil.component.scss']
+	selector: 'app-editar-perfil',
+	templateUrl: './editar-perfil.component.html',
+	styleUrls: ['./editar-perfil.component.scss'],
 })
 export class EditarPerfilComponent implements OnInit {
+	user!: User;
+	defaultImage = '../../../../assets/manutencao.jpg';
 
-  constructor() { }
+	constructor(private authService: AuthService, private router: Router) {}
 
-  ngOnInit(): void {
-  }
-
+	ngOnInit(): void {
+		this.authService.afAuth.onAuthStateChanged(user => {
+			if (user && user.email) {
+				console.log('CARAIO	' + user.email);
+				this.user = this.authService.getUserData(user.email);
+			}
+		});
+	}
 }
