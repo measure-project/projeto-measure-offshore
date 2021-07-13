bf87d2d005648759e6f932a8de60e0e8b6a62f35
Adriano Maretti <dri.cmaretti@gmail.com>
2021-07-13 15:56:37 -0300

    avanços na tela de edição de perfil

--- src/app/components/Perfil-Components/editar-perfil/editar-perfil.component.ts
+++ src/app/components/Perfil-Components/editar-perfil/editar-perfil.component.ts
@@ -15,6 +15,22 @@ export class EditarPerfilComponent implements OnInit {
 	constructor(private authService: AuthService, private router: Router) {}
 
 	ngOnInit(): void {
-		this.user = JSON.parse(localStorage.getItem('currentUser') || '{}');
+		this.authService.afAuth.onAuthStateChanged((user) => {
+			if (user)
+				this.user = JSON.parse(
+					localStorage.getItem('currentUser') || '{}'
+				);
+			else this.router.navigate(['/login']);
+		});
+	}
+
+	editUserData() {
+		this.authService.SetUserData(this.user);
+		this.router.navigate(['/verPerfil'])
+		console.log('editado');
+	}
+
+	editPhoto() {
+		this.router.navigate(['/editarPerfil']);
 	}
 }
