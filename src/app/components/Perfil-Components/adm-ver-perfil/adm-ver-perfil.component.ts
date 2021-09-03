8a90a14dda875fbffa422acb527d6e71651fa0cd
unknown <dri.cmaretti@gmail.com>
2021-09-03 18:17:54 -0300

    correção de bugs no editar adm

--- src/app/components/Perfil-Components/adm-ver-perfil/adm-ver-perfil.component.ts
+++ src/app/components/Perfil-Components/adm-ver-perfil/adm-ver-perfil.component.ts
@@ -1,37 +1,36 @@
-import { Router } from '@angular/router';
-import { AuthService } from './../../../services/auth.service';
-import { User } from './../../../models/user';
-import { Component, OnInit } from '@angular/core';
+import { Admin } from "./../../../models/admin";
+import { Router } from "@angular/router";
+import { AuthService } from "./../../../services/auth.service";
+import { User } from "./../../../models/user";
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-	selector: 'app-adm-ver-perfil',
-	templateUrl: './adm-ver-perfil.component.html',
-	styleUrls: ['./adm-ver-perfil.component.scss'],
+  selector: "app-adm-ver-perfil",
+  templateUrl: "./adm-ver-perfil.component.html",
+  styleUrls: ["./adm-ver-perfil.component.scss"],
 })
 export class AdmVerPerfilComponent implements OnInit {
-	user!: User;
-	defaultImage = '../../../../assets/manutencao.jpg';
+  admin!: Admin;
+  defaultImage = "../../../../assets/manutencao.jpg";
 
-	constructor(private router: Router, private authService: AuthService) {}
+  constructor(private router: Router, private authService: AuthService) {}
 
-	ngOnInit(): void {
-		this.authService.afAuth.onAuthStateChanged((user) => {
-			if (user)
-				this.user = JSON.parse(
-					localStorage.getItem('currentUser') || '{}'
-				);
-			else this.router.navigate(['/login']);
-		});
-	}
+  ngOnInit(): void {
+    this.authService.afAuth.onAuthStateChanged((user) => {
+      if (user)
+        this.admin = JSON.parse(localStorage.getItem("currentUser") || "{}");
+      else this.router.navigate(["/login"]);
+    });
+  }
 
-	// Por alguma razão, essa função não funciona e retorna um erro de "não é possível ler name de undefined no console". Precisamos checar depois
+  // Por alguma razão, essa função não funciona e retorna um erro de "não é possível ler name de undefined no console". Precisamos checar depois
 
-	toMembers() {
-		this.router.navigate(['/membros']);
-	}
+  toMembers() {
+    this.router.navigate(["/membros"]);
+  }
 
-	signOut() {
-		this.authService.SignOut();
-		this.router.navigate(['/login']);
-	}
+  signOut() {
+    this.authService.SignOut();
+    this.router.navigate(["/login"]);
+  }
 }
